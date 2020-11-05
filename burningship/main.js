4613fb2546d2ad3bdd6231c462a64f07d44b198f
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-11-05 22:51:35 +0900

    クリックで拡大

--- burningship/main.js
+++ burningship/main.js
@@ -1,51 +1,57 @@
+'use strict';
+
+const W = 800;
+const ZOOM = 0.8;
+
 const elementNumValue = (id) => {
   const el = document.getElementById(id);
   return parseFloat(el.value);
-}
+};
 
-const depth = (x, y) => {
+const setElementValue = (id, val) => {
+  const el = document.getElementById(id);
+  el.value = val;
+};
+
+
+const depth = (repeat, x, y) => {
   const cr = x;
   const ci = y;
   let zr = 0;
   let zi = 0;
-  for (let i = 0; i < 1000; ++i) {
+  for (let i = 0; i < repeat; ++i) {
     let azr = Math.abs(zr);
     let azi = Math.abs(zi);
     zr = azr * azr - azi * azi + cr;
     zi = 2 * azi * azr + ci;
     const d2 = zr * zr + zi * zi;
-    if (4 < d2) {
+    if (8 < d2) {
       return i;
     }
   }
   return Infinity;
 };
 
-const colorAt = (x, y) => {
-  const d = depth(x, y);
-  const c =
-    d == Infinity
-      ? 0
-      : (d & 0x7f) + 0x80;
-  return {
-    red: c,
-    green: c,
-    blue: c
-  };
+const colorAt = (repeat, x, y) => {
+  const d = depth(repeat, x, y);
+  return d == Infinity
+    ? 0
+    : ((((d | 0) * 41) & 0x7f) + 0x80) * 0x10101;
 };
 
 const draw = (ctx, cx, cy, width) => {
-  const W = 800;
   const data = new Uint8ClampedArray(W * W * 4);
   console.log("start drawing.");
+  const repeat = elementNumValue("repeat");
   for (let iy = 0; iy < W; ++iy) {
     for (let ix = 0; ix < W; ++ix) {
       let c = colorAt(
+        repeat,
         (ix / W - 0.5) * width + cx,
         (iy / W - 0.5) * width + cy);
-      data[(iy * W + ix) * 4 + 0] = c.red;
-      data[(iy * W + ix) * 4 + 1] = c.green;
-      data[(iy * W + ix) * 4 + 2] = c.blue;
+      data[(iy * W + ix) * 4 + 0] = c & 0xff;
+      data[(iy * W + ix) * 4 + 1] = (c >> 8) & 0xff;
+      data[(iy * W + ix) * 4 + 2] = (c >> 16) & 0xff;
       data[(iy * W + ix) * 4 + 3] = 256;
     }
   }
@@ -53,8 +59,9 @@ const draw = (ctx, cx, cy, width) => {
   ctx.putImageData(new ImageData(data, W, W), 0, 0);
 }
 
+const elCanvas = document.getElementById("canvas");
+
 const update = () => {
-  const elCanvas = document.getElementById("canvas");
   const ctx = elCanvas.getContext('2d');
   const cx = elementNumValue("x");
   const cy = elementNumValue("y");
@@ -63,7 +70,28 @@ const update = () => {
   draw(ctx, cx, cy, width);
 };
 
+const newCenter = (relativeFP, oldCenter, oldW, z) => {
+  let oldLeft = oldCenter - oldW / 2;
+  let fp = oldLeft + oldW * relativeFP;
+  let newLeft = (oldLeft - fp) * z + fp;
+  return newLeft + oldW * z / 2;
+
+};
+
+const onClickCanvas = (e) => {
+  const ix = e.offsetX;
+  const iy = e.offsetY;
+  const width = elementNumValue("width");
+  const cx = newCenter(ix / W, elementNumValue("x"), width, ZOOM);
+  const cy = newCenter(iy / W, elementNumValue("y"), width, ZOOM);
+  setElementValue("x", cx);
+  setElementValue("y", cy);
+  setElementValue("width", width * ZOOM);
+  update();
+};
+
 const main = () => {
+  elCanvas.addEventListener('click', onClickCanvas, false);
   document.getElementById("draw-button").onclick = () => {
     update();
   }
